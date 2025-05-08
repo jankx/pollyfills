6b56de2191f6484e68cf383b3924205bff6dafa5
Puleeno Nguyen <puleeno@gmail.com>
2025-05-08 10:15:17 +0700

    check wordpress version

--- wp/wordpress-6.4-functions.php
+++ wp/wordpress-6.4-functions.php
@@ -1,4 +1,13 @@
 <?php
+if (!defined('ABSPATH')) {
+	exit();
+}
+
+// Thoát nếu WordPress version >= 6.4
+if (version_compare(get_bloginfo('version'), '6.4', '>=')) {
+	return;
+}
+
 
 /**
  * Retrieves block types hooked into the given block, grouped by anchor block type and the relative position.
@@ -7,25 +16,28 @@
  *
  * @return array[] Array of block types grouped by anchor block type and the relative position.
  */
-function get_hooked_blocks() {
-	$block_types   = WP_Block_Type_Registry::get_instance()->get_all_registered();
-	$hooked_blocks = array();
-	foreach ( $block_types as $block_type ) {
-		if ( ! ( $block_type instanceof WP_Block_Type ) || ! is_array( $block_type->block_hooks ) ) {
-			continue;
-		}
-		foreach ( $block_type->block_hooks as $anchor_block_type => $relative_position ) {
-			if ( ! isset( $hooked_blocks[ $anchor_block_type ] ) ) {
-				$hooked_blocks[ $anchor_block_type ] = array();
+if (!function_exists('get_hooked_blocks')) {
+	function get_hooked_blocks()
+	{
+		$block_types = WP_Block_Type_Registry::get_instance()->get_all_registered();
+		$hooked_blocks = array();
+		foreach ($block_types as $block_type) {
+			if (!($block_type instanceof WP_Block_Type) || !is_array($block_type->block_hooks)) {
+				continue;
 			}
-			if ( ! isset( $hooked_blocks[ $anchor_block_type ][ $relative_position ] ) ) {
-				$hooked_blocks[ $anchor_block_type ][ $relative_position ] = array();
+			foreach ($block_type->block_hooks as $anchor_block_type => $relative_position) {
+				if (!isset($hooked_blocks[$anchor_block_type])) {
+					$hooked_blocks[$anchor_block_type] = array();
+				}
+				if (!isset($hooked_blocks[$anchor_block_type][$relative_position])) {
+					$hooked_blocks[$anchor_block_type][$relative_position] = array();
+				}
+				$hooked_blocks[$anchor_block_type][$relative_position][] = $block_type->name;
 			}
-			$hooked_blocks[ $anchor_block_type ][ $relative_position ][] = $block_type->name;
 		}
-	}
 
-	return $hooked_blocks;
+		return $hooked_blocks;
+	}
 }
 
 
@@ -64,42 +76,46 @@ function get_hooked_blocks() {
  *                                Its string return value will be appended to the serialized block markup.
  * @return string Serialized block markup.
  */
-function traverse_and_serialize_blocks( $blocks, $pre_callback = null, $post_callback = null ) {
-	$result       = '';
-	$parent_block = null; // At the top level, there is no parent block to pass to the callbacks; yet the callbacks expect a reference.
+if (!function_exists('traverse_and_serialize_blocks')) {
+	function traverse_and_serialize_blocks($blocks, $pre_callback = null, $post_callback = null)
+	{
+		$result = '';
+		$parent_block = null; // At the top level, there is no parent block to pass to the callbacks; yet the callbacks expect a reference.
 
-	$pre_callback_is_callable  = is_callable( $pre_callback );
-	$post_callback_is_callable = is_callable( $post_callback );
+		$pre_callback_is_callable = is_callable($pre_callback);
+		$post_callback_is_callable = is_callable($post_callback);
 
-	foreach ( $blocks as $index => $block ) {
-		if ( $pre_callback_is_callable ) {
-			$prev = 0 === $index
-				? null
-				: $blocks[ $index - 1 ];
+		foreach ($blocks as $index => $block) {
+			if ($pre_callback_is_callable) {
+				$prev = 0 === $index
+					? null
+					: $blocks[$index - 1];
 
-			$result .= call_user_func_array(
-				$pre_callback,
-				array( &$block, &$parent_block, $prev )
-			);
+				$result .= call_user_func_array(
+					$pre_callback,
+					array(&$block, &$parent_block, $prev)
+				);
+			}
+
+			if ($post_callback_is_callable) {
+				$next = count($blocks) - 1 === $index
+					? null
+					: $blocks[$index + 1];
+
+				$post_markup = call_user_func_array(
+					$post_callback,
+					array(&$block, &$parent_block, $next)
+				);
+			}
+
+			$result .= traverse_and_serialize_block($block, $pre_callback, $post_callback);
+			$result .= isset($post_markup) ? $post_markup : '';
 		}
 
-		if ( $post_callback_is_callable ) {
-			$next = count( $blocks ) - 1 === $index
-				? null
-				: $blocks[ $index + 1 ];
-
-			$post_markup = call_user_func_array(
-				$post_callback,
-				array( &$block, &$parent_block, $next )
-			);
-		}
-
-		$result .= traverse_and_serialize_block( $block, $pre_callback, $post_callback );
-		$result .= isset( $post_markup ) ? $post_markup : '';
+		return $result;
 	}
-
-	return $result;
 }
+
 
 /**
  * Traverses a parsed block tree and applies callbacks before and after serializing it.
@@ -135,52 +151,55 @@ function traverse_and_serialize_blocks( $blocks, $pre_callback = null, $post_cal
  *                                Its string return value will be appended to the serialized block markup.
  * @return string Serialized block markup.
  */
-function traverse_and_serialize_block( $block, $pre_callback = null, $post_callback = null ) {
-	$block_content = '';
-	$block_index   = 0;
+if (!function_exists('traverse_and_serialize_block')) {
+	function traverse_and_serialize_block($block, $pre_callback = null, $post_callback = null)
+	{
+		$block_content = '';
+		$block_index = 0;
 
-	foreach ( $block['innerContent'] as $chunk ) {
-		if ( is_string( $chunk ) ) {
-			$block_content .= $chunk;
-		} else {
-			$inner_block = $block['innerBlocks'][ $block_index ];
+		foreach ($block['innerContent'] as $chunk) {
+			if (is_string($chunk)) {
+				$block_content .= $chunk;
+			} else {
+				$inner_block = $block['innerBlocks'][$block_index];
 
-			if ( is_callable( $pre_callback ) ) {
-				$prev = 0 === $block_index
-					? null
-					: $block['innerBlocks'][ $block_index - 1 ];
+				if (is_callable($pre_callback)) {
+					$prev = 0 === $block_index
+						? null
+						: $block['innerBlocks'][$block_index - 1];
 
-				$block_content .= call_user_func_array(
-					$pre_callback,
-					array( &$inner_block, &$block, $prev )
-				);
+					$block_content .= call_user_func_array(
+						$pre_callback,
+						array(&$inner_block, &$block, $prev)
+					);
+				}
+
+				if (is_callable($post_callback)) {
+					$next = count($block['innerBlocks']) - 1 === $block_index
+						? null
+						: $block['innerBlocks'][$block_index + 1];
+
+					$post_markup = call_user_func_array(
+						$post_callback,
+						array(&$inner_block, &$block, $next)
+					);
+				}
+
+				$block_content .= traverse_and_serialize_block($inner_block, $pre_callback, $post_callback);
+				$block_content .= isset($post_markup) ? $post_markup : '';
+
+				++$block_index;
 			}
-
-			if ( is_callable( $post_callback ) ) {
-				$next = count( $block['innerBlocks'] ) - 1 === $block_index
-					? null
-					: $block['innerBlocks'][ $block_index + 1 ];
-
-				$post_markup = call_user_func_array(
-					$post_callback,
-					array( &$inner_block, &$block, $next )
-				);
-			}
-
-			$block_content .= traverse_and_serialize_block( $inner_block, $pre_callback, $post_callback );
-			$block_content .= isset( $post_markup ) ? $post_markup : '';
-
-			++$block_index;
 		}
-	}
 
-	if ( ! is_array( $block['attrs'] ) ) {
-		$block['attrs'] = array();
-	}
+		if (!is_array($block['attrs'])) {
+			$block['attrs'] = array();
+		}
 
-	return get_comment_delimited_block_content(
-		$block['blockName'],
-		$block['attrs'],
-		$block_content
-	);
+		return get_comment_delimited_block_content(
+			$block['blockName'],
+			$block['attrs'],
+			$block_content
+		);
+	}
 }
